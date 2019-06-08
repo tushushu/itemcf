a585e6d7b887462d911f83253e82a64468c5af50
tushushu <m18591915960@163.com>
2019-06-08 12:36:58 +0800

    完成load data模块

--- pyrecall/utils/load_data.py
+++ pyrecall/utils/load_data.py
@@ -3,18 +3,25 @@
 @Author: tushushu
 @Date: 2019-06-08 09:31:13
 """
+import os
+from collections import defaultdict
+from typing import Dict, Set, Tuple
+
 import numpy as np
 from numpy import ndarray
-from collections import defaultdict
-from typing import Dict, Set
-import os
+
 os.chdir(os.path.split(os.path.realpath(__file__))[0])
 BASE_PATH = os.path.abspath("..")
 PATH = os.path.join(BASE_PATH, "dataset", "movie_ratings.csv")
 
 
-def load_movie_ratings()->tuple:
-    """读取用户对电影评分的数据。"""
+def load_data()->Tuple[ndarray, ndarray]:
+    """读取用户对电影评分的数据。
+
+    Returns:
+        data {ndarray} -- 用户对哪些电影进行了评分，列名称[uid(int), item_id(int)]
+        label {ndarray} -- 用户对电影的评分，列名称[rating(float)]
+    """
 
     data = np.loadtxt(PATH, delimiter=',')
     data, label = data[:, :-1], data[:, -1]
@@ -22,9 +29,31 @@ def load_movie_ratings()->tuple:
     return data, label
 
 
-def get_user_rating_history(data: ndarray)->Dict[int, Set[int]]:
-    pass
+def load_ucf_data()->Dict[int, Set[int]]:
+    """读取用户对电影评分的数据，适用于User CF算法。
+
+    Returns:
+        Dict[int, Set[int]] -- uid及其对应的item_id。
+    """
+
+    data, _ = load_data()
+    ret = defaultdict(set)  # type: defaultdict
+    for uid, item_id in data:
+        ret[uid].add(item_id)
+
+    return ret
 
 
-def get_item_rated_history():
-    pass
+def load_icf_data()->Dict[int, Set[int]]:
+    """读取用户对电影评分的数据，适用于Item CF算法。
+
+    Returns:
+        Dict[int, Set[int]] -- item_id及其对应的uid。
+    """
+
+    data, _ = load_data()
+    ret = defaultdict(set)  # type: defaultdict
+    for uid, item_id in data:
+        ret[item_id].add(uid)
+
+    return ret
