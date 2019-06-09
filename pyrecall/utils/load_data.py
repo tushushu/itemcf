4378c2f14f0d8022d4886114d8ac662a53fc263b
tushushu <m18591915960@163.com>
2019-06-09 10:15:19 +0800

    load_data改用pandas替代numpy读取csv文件

--- pyrecall/utils/load_data.py
+++ pyrecall/utils/load_data.py
@@ -5,37 +5,27 @@
 """
 import os
 from collections import defaultdict
-from typing import Dict, Set, Tuple
+from typing import Dict, Set
 
-import numpy as np
-from numpy import ndarray
+import pandas as pd
+from pandas import DataFrame
 
 os.chdir(os.path.split(os.path.realpath(__file__))[0])
 BASE_PATH = os.path.abspath("..")
 PATH = os.path.join(BASE_PATH, "dataset", "movie_ratings.csv")
 
 
-def load_movie_ratings(with_timestamp=False)->Tuple[ndarray, ndarray]:
+def load_movie_ratings()->DataFrame:
     """读取用户对电影评分的数据。
-
-    Keyword Arguments:
-        with_timestamp {bool} -- data中是否带上time_stamp。 (default: {False})
+    列名称[uid(int), item_id(int), rating(float), timestamp(float)]
 
     Returns:
-        data {ndarray} -- 用户对哪些电影进行了评分，列名称[uid(int), item_id(int),
-                          timestamp(float)]
-        label {ndarray} -- 用户对电影的评分，列名称[rating(float)]
+        data {ndarray}
     """
 
-    movie_ratings = np.loadtxt(PATH, delimiter=',')
-    data_col_indexes = [0, 1]
-    label_col_index = 2
-    if with_timestamp:
-        data_col_indexes.append(3)
-    data = movie_ratings[:, data_col_indexes]
-    label = movie_ratings[:, label_col_index]
-
-    return data, label
+    data = pd.read_csv(PATH)
+    data.columns = ["uid", "item_id", "rating", "timestamp"]
+    return data
 
 
 def load_ucf_data()->Dict[int, Set[int]]:
@@ -45,9 +35,9 @@ def load_ucf_data()->Dict[int, Set[int]]:
         Dict[int, Set[int]] -- uid及其对应的item_id。
     """
 
-    data, _ = load_movie_ratings()
+    data = load_movie_ratings().loc[:, ["uid", "item_id"]]
     ret = defaultdict(set)  # type: defaultdict
-    for uid, item_id in data:
+    for _, uid, item_id in data.itertuples():
         ret[uid].add(item_id)
 
     return ret
@@ -60,9 +50,9 @@ def load_icf_data()->Dict[int, Set[int]]:
         Dict[int, Set[int]] -- item_id及其对应的uid。
     """
 
-    data, _ = load_movie_ratings()
+    data = load_movie_ratings().loc[:, ["uid", "item_id"]]
     ret = defaultdict(set)  # type: defaultdict
-    for uid, item_id in data:
+    for _, uid, item_id in data.itertuples():
         ret[item_id].add(uid)
 
     return ret
