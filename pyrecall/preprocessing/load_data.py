51080fedfe6d97164848314c08d0551ebfc4c879
tushushu <m1859191596@163.com>
2019-10-15 15:37:13 +0800

    加载电影评分数据

---
+++ pyrecall/preprocessing/load_data.py
@@ -0,0 +1,43 @@
+"""
+@Author: tushushu
+@Date: 2019-10-15 15:21:17
+"""
+import os
+import pandas as pd
+from pandas import DataFrame
+
+
+os.chdir(os.path.split(os.path.realpath(__file__))[0])
+BASE_PATH = os.path.abspath("..")
+
+
+class MovieRatingsData:
+    """读取电影评分的数据。
+
+    Attributes:
+        user_col {str} -- 用户id对应的列名称。
+        item_col {str} -- 物品id对应的列名称。
+        rating_col {str} -- 用户对物品的评分对应的列名称。
+        file_name {str} -- 数据文件名称。
+        data {DataFrame} -- 数据。
+    """
+
+    def __init__(self):
+        self.user_col = "userId"
+        self.item_col = "movieId"
+        self.rating_col = "rating"
+        self.file_name = "movie_ratings"
+        self.data = self.load()
+
+    def __str__(self):
+        return self.data.head(3)
+
+    def load(self) -> DataFrame:
+        """读取数据。
+
+        Returns:
+            DataFrame
+        """
+        path = os.path.join(BASE_PATH, "dataset", "%s.csv" % self.file_name)
+        data = pd.read_csv(path)
+        return data
